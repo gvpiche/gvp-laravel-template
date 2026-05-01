797b519a7de40e4e40caa2512a1b5f56fe5aea57
Gabriel Vendrame-Piche <gabriel_vpiche@hotmail.com>
2026-05-01 19:38:25 -0400

    Remove welcome page

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,17 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+use App\Models\User;
 
-    $response->assertStatus(200);
+it('redirects guests from home to login', function () {
+    $this->get('/')
+        ->assertRedirect(route('login'));
+});
+
+it('redirects authenticated users from home to dashboard', function () {
+    $this->actingAs(new User([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+    ]))
+        ->get('/')
+        ->assertRedirect(route('dashboard'));
 });
